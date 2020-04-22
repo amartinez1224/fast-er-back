d64557009e8a31ddbffdfcd5a832816c2dbf6141
Andres <a.martinez@uniandes.edu.co>
2020-04-22 18:38:49 -0500

    get position and return hospitals in order

--- back.py
+++ back.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask,request
 from flask_restful import Api, Resource, reqparse
 import mysql.connector
+import numpy as np
 
 app = Flask(__name__)
 api = Api(app)
@@ -64,6 +65,16 @@ def getEspecialidadesHospital(connectdb,id):
         esp.append(e)
     return esp
 
+def distancia(lat1,lon1,lat2,lon2):
+  R = 6371
+  dLat = np.radians(lat2-lat1)
+  dLon = np.radians(lon2-lon1);
+  a = np.sin(dLat/2) * np.sin(dLat/2) + np.cos(np.radians(lat1)) * np.cos(np.radians(lat2)) * np.sin(dLon/2) * np.sin(dLon/2)
+  c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
+  d = R * c
+  return d
+
+
 
 connectdb = mysql.connector.connect(
   host="XXXXXXXXXX",
@@ -74,9 +85,13 @@ connectdb = mysql.connector.connect(
 )
 
 class Hospitales(Resource):
-    def get(self):
+    def get(self,lat,lang):
         global connectdb
         hosp = getHospitalesFull(connectdb)
+        for h in hosp:
+            dis = distancia(float(lat),float(lang), h["lat"], h["lng"])
+            h["distancia"]=dis
+        hosp = sorted(hosp, key=lambda x: x["distancia"])
         return hosp,200
 
 @app.after_request
@@ -84,5 +99,5 @@ def add_security_headers(resp):
     resp.headers['Access-Control-Allow-Origin']='*'
     return resp
 
-api.add_resource(Hospitales,"/hospitales")
+api.add_resource(Hospitales,"/hospitales/<lat>/<lang>")
 app.run(host="0.0.0.0",debug=True)
